13e43813fc60cf8db94a36d86542cd3eab398c20
cwesley8 <cwesley8@uncc.edu>
2016-08-10 22:11:05 -0400

    Updates!
    Added ranks and tasks for adventurers

--- Assets/RootGameScreen/Scripts/StaticData.cs
+++ Assets/RootGameScreen/Scripts/StaticData.cs
@@ -5,9 +5,21 @@ using System.Text;
 
 public class StaticData
 {  
-    static public int savedTotalGold = 0;
+    static public int savedTotalGold = 10;
     static public int savedGoldPerSecond = 0;
     static public int savedWarriorLevel = 0;
-    static public int savedRrogueLevel = 0;
+    static public int savedRogueLevel = 0;
+    static public int savedWarriorRank = 0;
+    static public int savedRogueRank = 0;
+
+    static public bool warriorUpPressed = false;
+    static public bool rogueUpPressed = false;
+
+    static public string[] warriorStatuses = new string[2] {"Mug children",
+                                                            "Bodyguard Merchant"};
+
+    static public string[] rogueStatuses = new string[2] {"Pickpocket grannies",
+                                                          "Ambush caravans"};
+
 
 }
